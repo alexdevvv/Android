5d110ab06a31bdc22131f16b9a987206ac0d17dd
Aleksandr Sergeev <alex.dev.sergeev@gmail.com>
2021-10-07 21:56:25 +0300

    Timer first commit

---
+++ Timer/app/src/main/java/com/example/timer/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.timer;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView timerTextView;
+    private Button startButton;
+    private Button pauseButton;
+    private Button resetButton;
+    private int second = 0;
+    private boolean isRunning = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        timerTextView = findViewById(R.id.timer_tv);
+        if (savedInstanceState != null) {
+            second = savedInstanceState.getInt("seconds");
+            isRunning = savedInstanceState.getBoolean("isRunning");
+        }
+        initStartButton();
+        initPauseButton();
+        initResetButton();
+        runTimer();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("seconds", second);
+        outState.putBoolean("isRunning", isRunning);
+    }
+
+    private void initStartButton() {
+        startButton = findViewById(R.id.start_bt);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isRunning = true;
+
+            }
+        });
+    }
+
+    private void initPauseButton() {
+        pauseButton = findViewById(R.id.pause_bt);
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isRunning = false;
+            }
+        });
+    }
+
+    private void initResetButton() {
+        resetButton = findViewById(R.id.reset_bt);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isRunning = false;
+                second = 0;
+            }
+        });
+    }
+
+    private void runTimer() {
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = second / 3600;
+                int minutes = (second % 3600) / 60;
+                int secs = second % 60;
+
+                String time = String.format("%d:%02d:%02d", hours, minutes, secs);
+                timerTextView.setText(time);
+                if (isRunning) {
+                    second++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+
+        });
+
+    }
+
+
+}
