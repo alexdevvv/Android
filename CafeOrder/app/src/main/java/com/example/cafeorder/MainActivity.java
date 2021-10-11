eeddc0f37f32470a37092c8d489737e3e2c7d7fe
Aleksandr Sergeev <alex.dev.sergeev@gmail.com>
2021-10-11 11:45:09 +0300

    Create order commit

---
+++ CafeOrder/app/src/main/java/com/example/cafeorder/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.cafeorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editTextUserName;
+    EditText editTextUserPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initLoginButton();
+    }
+
+    void initLoginButton() {
+        Button button = findViewById(R.id.create_order_bt);
+        editTextUserName = findViewById(R.id.user_name_et);
+        editTextUserPassword = findViewById(R.id.user_password_et);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (editTextUserName.getText().length() != 0 && editTextUserPassword.getText().length() != 0) {
+                    Intent intent = new Intent(getApplicationContext(), OrderActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Все поля должны быть заполнены.", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+    }
+}
